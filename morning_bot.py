8f6a66684c10c30a42388924bcbc83ba81b783a2
windtrue12-gif <windtrue12@gmail.com>
2026-07-25 15:47:25 +0900

    Update morning_bot.py

--- morning_bot.py
+++ morning_bot.py
@@ -89,7 +89,6 @@ def get_reminders():
     response = notion.data_sources.query(
         data_source_id=REMINDER_DATA_SOURCE_ID
     )
-
 	today = datetime.now(ZoneInfo("Asia/Tokyo"))
 
 	week_map = {
